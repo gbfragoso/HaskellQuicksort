99b3cf45ef47b12f4c6d0ce72fb583ca9ac0d237
Gustavo Fragoso <gbfragoso94@gmail.com>
2016-06-09 16:19:11 -0300

    Add files via upload

---
+++ quicksort6.hs
@@ -0,0 +1,19 @@
+import Data.Time
+
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater)
+    where
+        lesser  = filter (< p) xs
+        greater = filter (>= p) xs
+
+wordtolist string = map read $ words string :: [Int]
+
+main = do
+          start <- getCurrentTime
+          vet <- readFile "permutation_n6.txt"                            
+          let a = lines vet
+          let b = map(wordtolist) a
+          let c = map(quicksort) b
+          stop <- getCurrentTime
+          print $ diffUTCTime stop start
